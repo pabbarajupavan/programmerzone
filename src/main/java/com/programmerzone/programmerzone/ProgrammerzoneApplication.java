617356ba555a37c273f1b44b9b6a9cfe7ea211c4
pabbarajupavan <pavankumar24@live.com>
2023-02-06 11:16:23 +0530

    project created

---
+++ src/main/java/com/programmerzone/programmerzone/ProgrammerzoneApplication.java
@@ -0,0 +1,13 @@
+package com.programmerzone.programmerzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammerzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammerzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmerzone/programmerzone/ProgrammerzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmerzone.programmerzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
